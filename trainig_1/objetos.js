af8c0d5e61d69724b79ba2685e5394476e28e5f1
Daniel Martínez <39061102+Danie1M@users.noreply.github.com>
2023-06-07 01:55:41 -0600

    tercero

---
+++ trainig_1/objetos.js
@@ -0,0 +1,40 @@
+
+/// 1.-
+
+let libro = {
+    titulo:"Origen",
+    autor:"Dan Brown",
+    año:2019
+}
+
+/// 2.-
+console.log("titulo: ", libro.titulo)
+console.log("autor: ", libro.autor)
+console.log("año: ",libro.año)
+
+/// 3.-
+libro.año = 2020
+console.log("3.- ",libro)
+
+/// 4.-
+libro["páginas"] = 640
+console.log("4.- ",libro)
+
+/// 5.-
+delete libro.páginas
+console.log("5.- ",libro)
+
+/// 6.-
+for(let ind in libro){
+    console.log(ind,": ",libro[ind])
+}
+
+/// 7.-
+libro = function descripcion() { 
+    const descr = "El libro " +this.titulo +", genera consiencias sobre como los cambios que se viven día a dia"
+    console.log(descr)
+}
+
+console.log(libro)
+
+/// 8.-
